4fd113f566f8c881a338ded848a1014b5574a5bb
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-02-21 20:59:44 +0200

    Add currency controller to currency.route.js

--- backend/routes/currency.route.js
+++ backend/routes/currency.route.js
@@ -1,15 +1,8 @@
 import express from 'express';
-import { mongodbConnect } from '../config/db.js';
+import { currencyController } from '../controllers/currency.controller.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  try {
-    mongodbConnect();
-    res.status(200).json({ message: 'Test completed' });
-  } catch (error) {
-    res.status(500).json({ message: 'Server Error' });
-  }
-});
+router.get('/', currencyController);
 
 export default router;
