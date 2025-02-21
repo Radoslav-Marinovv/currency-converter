1a883765497003dfe7cba4a03e8f58c89fa1f627
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-02-21 18:38:10 +0200

    Add server with express and test get request

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  try {
+    res.status(200).json({ message: 'Test completed' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+})
