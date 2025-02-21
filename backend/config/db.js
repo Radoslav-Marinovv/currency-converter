18cdac0fe4e6d55991c41bb47ca04642b18e1ff4
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-02-21 20:19:28 +0200

    Add db connection function to config/db.js

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+export const mongodbConnect = async () => {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(process.env.MONGO_URI, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+    console.log("Connection to MongoDB Closed");
+  }
+};
+
