aaf814824d1638520066208c090b4d76e4215359
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-02-21 20:48:28 +0200

    Add currency routes to server and remove test endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { mongodbConnect } from './config/db.js';
+import currencyRoutes from './routes/currency.route.js';
 
 dotenv.config();
 
@@ -10,15 +10,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.use('/api/currency', currencyRoutes)
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-app.get('/', (req, res) => {
-  try {
-    mongodbConnect();
-    res.status(200).json({ message: 'Test completed' });
-  } catch (error) {
-    res.status(500).json({ message: 'Server Error' });
-  }
-})
