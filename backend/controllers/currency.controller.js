3e68211df7bd7dfab0ac0412dc986afa0ac5abc6
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-02-22 19:09:46 +0200

    Refactor currency controller to include mongodbDisconnect and fetch all currencies

--- backend/controllers/currency.controller.js
+++ backend/controllers/currency.controller.js
@@ -1,10 +1,16 @@
-import { mongodbConnect } from "../config/db.js";
+import { mongodbConnect, mongodbDisconnect } from "../config/db.js";
+import Currency from "../models/currency.model.js";
 
-export const currencyController = (req, res) => {
+export const getAllCurrenciesController = async (req, res) => {
   try {
-    mongodbConnect();
-    res.status(200).json({ message: 'Test completed' });
+    await mongodbConnect();
+    const currencies = await Currency.find();
+    !currencies ?
+      res.status(404).json({ message: 'No currencies found', currencies: {} }) :
+      res.status(200).json({ message: 'Currency controller', currencies });
   } catch (error) {
     res.status(500).json({ message: 'Server Error' });
+  } finally {
+    await mongodbDisconnect();
   }
 };
