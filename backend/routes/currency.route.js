0f19f20449609595c509f99f884117f726f4023c
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-02-22 19:14:48 +0200

    Add createCurrency route

--- backend/routes/currency.route.js
+++ backend/routes/currency.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getAllCurrenciesController } from '../controllers/currency.controller.js';
+import { createCurrencyController, getAllCurrenciesController } from '../controllers/currency.controller.js';
 
 const router = express.Router();
 
 router.get('/', getAllCurrenciesController);
+router.post('/', createCurrencyController);
 
 export default router;
