7e3165a97b39a4295f6d262d146fa40608d0f312
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-02-22 18:31:39 +0200

    Refactor mongodbConnect in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,14 +4,11 @@ const clientOptions = { serverApi: { version: '1', strict: true, deprecationErro
 
 export const mongodbConnect = async () => {
   try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
     await mongoose.connect(process.env.MONGO_URI, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
+    console.log("You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB: ", error.message);
     await mongoose.disconnect();
     console.log("Connection to MongoDB Closed");
   }
 };
-
