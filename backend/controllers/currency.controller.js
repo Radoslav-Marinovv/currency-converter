ef7d793825d1b3a38f9df01a992cdf49f105d6d3
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-02-22 21:58:23 +0200

    Refactor database connection in db.js, currency controllers, moved mongodbConnect function to server.js

--- backend/controllers/currency.controller.js
+++ backend/controllers/currency.controller.js
@@ -1,17 +1,13 @@
-import { mongodbConnect, mongodbDisconnect } from "../config/db.js";
 import Currency from "../models/currency.model.js";
 
 export const getAllCurrenciesController = async (req, res) => {
   try {
-    await mongodbConnect();
     const currencies = await Currency.find();
     !currencies ?
       res.status(404).json({ message: 'No currencies found', currencies: {} }) :
       res.status(200).json({ message: 'Currency controller', currencies });
   } catch (error) {
     res.status(500).json({ message: 'Server Error' });
-  } finally {
-    await mongodbDisconnect();
   }
 };
 
@@ -27,15 +23,9 @@ export const createCurrencyController = async (req, res) => {
     };
 
     const newCurrency = new Currency(currency);
-
-    await mongodbConnect()
     await newCurrency.save();
-
     res.status(201).json({ message: 'Currency created successfully', currency: newCurrency });
   } catch (error) {
     res.status(500).json({ message: `Server Error: ${error.message}` });
-  } finally {
-    await mongodbDisconnect();
   }
 };
-
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
+import { mongodbConnect } from './config/db.js';
 import currencyRoutes from './routes/currency.route.js';
 
 dotenv.config();
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use('/api/currency', currencyRoutes);
 
 app.listen(PORT, () => {
+  mongodbConnect();
   console.log(`Server is running on port ${PORT}`);
 });
 
