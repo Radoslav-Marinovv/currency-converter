7f06490854774843e0a5bc6e86031ae8ecce7658
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-02-24 00:41:26 +0200

    Add CORS middleware to server and update dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import { mongodbConnect } from './config/db.js';
 import currencyRoutes from './routes/currency.route.js';
@@ -10,6 +11,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
 app.use('/api/currency', currencyRoutes);
