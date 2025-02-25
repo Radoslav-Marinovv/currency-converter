0a81e2f526b864519656fc8489c95fc0348f1dba
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-02-25 15:46:57 +0200

    Add try catch block for getAllCurrenciesAsync, Add type CurrencyType for the response object

--- frontend/src/services/currency.service.ts
+++ frontend/src/services/currency.service.ts
@@ -1,21 +1,33 @@
 import { CurrencyState } from '../state/currency/currencySlice';
 
+type CurrencyType = {
+  currencies: CurrencyState[];
+};
+
 const BASE_URL =
   import.meta.env.VITE_BASE_URL || 'http://localhost:5000/api/currency';
 
 export const getAllCurrenciesAsync = async () => {
-  const response = await fetch(BASE_URL, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': `${BASE_URL}`,
-    },
-  });
-  const responseToJSON = await response.json();
-  const convertToArray = responseToJSON.currencies.map(
-    (currency: CurrencyState) => {
+  try {
+    const response = await fetch(BASE_URL, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': `${BASE_URL}`,
+      },
+    });
+    const responseToJSON = (await response.json()) as CurrencyType;
+    const convertToArray = responseToJSON.currencies.map((currency) => {
       return currency;
+    });
+    return convertToArray;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Error fetching data: ', error.message);
+    } else {
+      console.error('Error fetching data: ', error);
     }
-  );
-  return convertToArray;
+
+    return [];
+  }
 };
