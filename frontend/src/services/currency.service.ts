6ec89455bcc2662252ca41dcca56d55b79a62365
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-02-28 16:22:32 +0200

    Refactor getAllCurrenciesAsync fn created fetchCurrencyData and removed unnecessary map over responseToJSON

--- frontend/src/services/currency.service.ts
+++ frontend/src/services/currency.service.ts
@@ -7,27 +7,26 @@ type CurrencyType = {
 const BASE_URL =
   import.meta.env.VITE_BASE_URL || 'http://localhost:5000/api/currency';
 
+const fetchCurrencyData = async () =>
+  fetch(BASE_URL, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': `${BASE_URL}`,
+    },
+  });
+
 export const getAllCurrenciesAsync = async () => {
   try {
-    const response = await fetch(BASE_URL, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': `${BASE_URL}`,
-      },
-    });
+    const response = await fetchCurrencyData();
     const responseToJSON = (await response.json()) as CurrencyType;
-    const convertToArray = responseToJSON.currencies.map((currency) => {
-      return currency;
-    });
-    return convertToArray;
+    return responseToJSON.currencies;
   } catch (error) {
     if (error instanceof Error) {
       console.error('Error fetching data: ', error.message);
     } else {
       console.error('Error fetching data: ', error);
     }
-
     return [];
   }
 };
