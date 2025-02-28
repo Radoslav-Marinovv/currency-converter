6b80b6e2dbcdcf3660667ea85c6a9eb956ead55b
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-02-28 17:25:07 +0200

    Refactor HomePage component to use TopTenCurrencies for displaying top 10 currencies

--- frontend/src/components/pages/home/HomePage.tsx
+++ frontend/src/components/pages/home/HomePage.tsx
@@ -1,19 +1,22 @@
-import { useDispatch, useSelector } from "react-redux"
-import { AppDispatch, RootState } from "../../../state/store"
 import { useEffect } from "react";
-import { CurrencyState, getInitialState } from "../../../state/currency/currencySlice";
-import { setStoreCurrentTime, shouldUpdate } from "../../../helper/date/date.helper";
-import { TIME_TO_UPDATE_IN_MS } from "../../../constants/constants";
 import { Link } from "react-router";
 
-const HomePage = () => {
+import { useDispatch, useSelector } from "react-redux"
+import { AppDispatch, RootState } from "../../../state/store"
 
+import TopTenCurrencies from "../../top-ten-currencies/TopTenCurrencies";
+
+import { TIME_TO_UPDATE_IN_MS } from "../../../constants/constants";
+import { setStoreCurrentTime, shouldUpdate } from "../../../helper/date/date.helper";
+import { CurrencyState, getInitialState } from "../../../state/currency/currencySlice";
+
+const HomePage = () => {
   const currencies: CurrencyState[] = useSelector((state: RootState) => state.currencies || []);
   const dispatch = useDispatch<AppDispatch>();
 
   const TOP_TEN_CURRENCIES = ['USD', 'EUR', 'BGN', 'JPY', 'GBP', 'AUD', 'CAD', 'CHF', 'CNY', 'SEK'];
 
-  const top10Currencies = currencies.filter((currency: CurrencyState) => {
+  const topTenCurrencies = currencies.filter((currency: CurrencyState) => {
     if (TOP_TEN_CURRENCIES.includes(currency.nameShort)) {
       return currency;
     };
@@ -38,26 +41,7 @@ const HomePage = () => {
       <h1 className="text-4xl">Currency Converter</h1>
       <p className="text-2xl text-warning">NON COMMERCIAL PROJECT - USED FOR EDUCATIONAL PURPOSES ONLY</p>
       <p className="text-2xl">API used:  <Link to={'https://freecurrencyapi.com/'} target="_blank" rel="noopener noreferrer">https://freecurrencyapi.com/</Link></p>
-
-      <section className="rounded-2xl shadow-accent-content shadow-2xl">
-        <h2 className="flex flex-col text-3xl p-3">Top 10 currencies</h2>
-        <p className="text-xl">Base: USD</p>
-        <ul className="flex flex-col gap-3 align-middle justify-evenly p-2">
-          {
-            top10Currencies.length > 0 ? top10Currencies.map((currency: CurrencyState) => {
-              return (
-                <li key={currency._id} className="flex flex-row items-center justify-between odd:bg-base-200 even:bg-base-100 p-2 rounded-2xl shadow-accent-content shadow-2xl">
-                  {currency.nameShort} - {currency.nameFull}, {currency.exchangeRateToOneUSD}
-                  <img src={currency.countryFlag} alt={currency.nameFull} className="w-8 h-8" />
-                </li>
-              )
-            }) :
-              <div className="flex justify-center items-center h-96">
-                <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-secondary"></div>
-              </div>
-          }
-        </ul>
-      </section>
+      <TopTenCurrencies currencies={topTenCurrencies} />
     </div >
   )
 }
