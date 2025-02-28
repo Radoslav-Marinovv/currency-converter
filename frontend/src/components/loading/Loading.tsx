030ac95eb6c0023d802965994fee7ffaec4077b6
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-02-28 17:24:24 +0200

    Add Loading component with spinner animation

---
+++ frontend/src/components/loading/Loading.tsx
@@ -0,0 +1,8 @@
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-96">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-secondary"></div>
+    </div>
+  )
+}
+export default Loading;
