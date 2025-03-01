4a02e2dca9ba3a91844665d06c7617fdfddcf240
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-03-01 13:48:05 +0200

    Add exchange reducer to the store configuration

--- frontend/src/state/store.ts
+++ frontend/src/state/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import currencyReducer from './currency/currencySlice';
+import exchangeReducer from './exchange/exchangeSlice';
 
 export const store = configureStore({
   reducer: {
     currencies: currencyReducer,
+    exchange: exchangeReducer,
   },
 });
 
