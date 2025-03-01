5ba5e843b0f9e6d02cfd3923cb306b6b5955e5ae
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-03-01 17:00:42 +0200

    Add NoCurrencies component to display message when no currencies are found

---
+++ frontend/src/components/no-currencies/NoCurrencies.tsx
@@ -0,0 +1,9 @@
+const NoCurrencies = () => {
+  return (
+    <div className="flex justify-center items-center text-2xl h-96">
+      <p>No currencies found</p>
+    </div>
+  );
+};
+
+export default NoCurrencies;
