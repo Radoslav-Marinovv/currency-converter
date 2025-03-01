d42248741dc2e1c6394d1d39dacc5e24f603f965
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-03-01 16:45:38 +0200

    Update MyListPage to use getMyList helper for retrieving currencies

--- frontend/src/components/pages/my-list/MyListPage.tsx
+++ frontend/src/components/pages/my-list/MyListPage.tsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../../state/store";
 
 import DisplayCurrencies from "../../DisplayCurrencies/DisplayCurrencies";
+import { getMyList } from "../../../helper/local-storage/localStorage.helper";
 
 const MyListPage = () => {
   const currencies = useSelector((state: RootState) => state.currencies || {});
@@ -9,7 +10,7 @@ const MyListPage = () => {
   return (
     <div className="flex flex-col gap-4 text-center">
       <h1 className="flex flex-col text-3xl p-3">My List</h1>
-      <DisplayCurrencies currencies={currencies} />
+      <DisplayCurrencies currencies={getMyList(currencies)} />
     </div>
   );
 };
