8917097d2955cbd9cbc654f67ba93ad1b88f19d1
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-03-01 15:01:22 +0200

    Add DisplayCurrencies component into MyListPage

--- frontend/src/components/pages/my-list/MyListPage.tsx
+++ frontend/src/components/pages/my-list/MyListPage.tsx
@@ -1,9 +1,17 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../../state/store";
+
+import DisplayCurrencies from "../../DisplayCurrencies/DisplayCurrencies";
+
 const MyListPage = () => {
+  const currencies = useSelector((state: RootState) => state.currencies || {});
+
   return (
-    <div>
-      <h1>My List</h1>
+    <div className="flex flex-col gap-4 text-center">
+      <h1 className="flex flex-col text-3xl p-3">My List</h1>
+      <DisplayCurrencies currencies={currencies} />
     </div>
   );
-}
+};
 
 export default MyListPage;
