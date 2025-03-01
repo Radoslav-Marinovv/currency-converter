764e6a90ceaa37cd932c34463ee28067f478d5c9
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-03-01 13:58:39 +0200

    Update ErrorNotFoundPage to use useNavigate for navigation

--- frontend/src/components/pages/error/ErrorNotFoundPage.tsx
+++ frontend/src/components/pages/error/ErrorNotFoundPage.tsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router"
+import { Link, useNavigate } from "react-router"
 
 const ErrorNotFoundPage = () => {
+  const navigation = useNavigate();
   return (
     <div className="flex flex-col justify-center h-lvh text-center gap-4">
       <h1 className="text-4xl">404 Not Found</h1>
       <p className="text-2xl">Sorry, the page you are looking for does not exist.</p>
-      <Link to={`/`} className="btn btn-wide self-center">Go back to the homepage</Link>
+      <Link to="#" onClick={() => navigation(-1)} className="btn btn-wide self-center">Go back to the homepage</Link>
     </div>
   )
 }
