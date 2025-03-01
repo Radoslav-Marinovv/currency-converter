5248b34f758e1ae57d649645c74728e3b6ff8fce
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-03-01 17:06:57 +0200

    Add BASE_CURRENCY_TEXT constant for base currency label

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -3,3 +3,5 @@ export const TIME_TO_UPDATE = 2;
 export const TIME_TO_UPDATE_IN_MS = 1000 * 60 * 60 * TIME_TO_UPDATE;
 
 export const BASE_CURRENCY = 'USD';
+
+export const BASE_CURRENCY_TEXT = 'Base: ';
