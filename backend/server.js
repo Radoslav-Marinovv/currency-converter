fe8bd8a0f6c584960bd1701470d2cfd3e75c2598
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-03-07 01:37:56 +0200

    Update static file serving paths in server.js for production environment

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,10 @@ app.use(express.json());
 app.use('/api/currency', currencyRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
   });
 };
 
