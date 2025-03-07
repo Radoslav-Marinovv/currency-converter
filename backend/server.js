19ba401d545538443e8aa5938d0e5eaa3aedbab6
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-03-07 12:42:01 +0200

    Fix static file path in production environment for backend frontend assets

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use('/api/currency', currencyRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './frontend/dist')));
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
