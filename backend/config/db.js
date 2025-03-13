8d9b45f8db1550020fc6c2a514e3f228c7cac3e1
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-03-13 18:08:23 +0200

    Refactor MongoDB connection to use a constant for the URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const MONGO_URI = process.env.MONGO_URI || '';
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 export const mongodbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, clientOptions);
+    await mongoose.connect(MONGO_URI, clientOptions);
     console.log("You successfully connected to MongoDB!");
   } catch (error) {
     console.error("Error connecting to MongoDB: ", error.message);
