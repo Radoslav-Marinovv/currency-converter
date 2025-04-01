2096d292aac579123b4770ce2cffdb932b461d79
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-04-01 18:30:06 +0300

    Add functions to manage currency list in local storage

--- src/helper/local-storage/localStorage.helper.ts
+++ src/helper/local-storage/localStorage.helper.ts
@@ -13,3 +13,22 @@ export const getMyList = (currencies: CurrencyState[]) => {
     }
   });
 };
+
+export const filterMyList = (currency: CurrencyState) => {
+  const idList = localStorage.getItem('myList') || '';
+
+  return idList.includes(currency._id);
+};
+
+export const addRemoveToMyList = (currencyId: string) => {
+  const idList = localStorage.getItem('myList') || '';
+  const newIdList = idList.length > 0 ? idList.split(',') : [];
+
+  if (newIdList.includes(currencyId)) {
+    newIdList.splice(newIdList.indexOf(currencyId), 1);
+  } else {
+    newIdList.push(currencyId);
+  }
+
+  localStorage.setItem('myList', newIdList.toString());
+};
