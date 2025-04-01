09323d3d42c7308d5df7243ff6c5306359d5edb2
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-04-01 19:11:34 +0300

    Refactor MyListPage to use Redux state for myList and filter currencies accordingly

--- src/components/pages/my-list/MyListPage.tsx
+++ src/components/pages/my-list/MyListPage.tsx
@@ -5,22 +5,24 @@ import DisplayCurrencies from "../../display-currencies/DisplayCurrencies";
 import CarouselCurrencies from "../../carousel-currencies/CarouselCurrencies";
 
 import { getMyList } from "../../../helper/local-storage/localStorage.helper";
-import { useEffect } from "react";
 
 const MyListPage = () => {
   const currencies = useSelector((state: RootState) => state.currencies || {});
-  const myList = localStorage.getItem('myList') || '';
+  const myList = useSelector((state: RootState) => state.myList || []);
 
-
-  useEffect(() => {
-
-  }, [myList]);
+  const myListCurrencies = currencies.filter((currency) => {
+    if (myList?.includes(currency._id)) {
+      return currency;
+    }
+  });
 
   return (
     <div className="flex flex-col gap-4" >
       <h1 className="flex flex-col text-3xl p-3">My List</h1>
       <CarouselCurrencies />
-      <DisplayCurrencies currencies={getMyList(currencies)} removeFromMyList />
+      {myListCurrencies.length > 0 ?
+        <DisplayCurrencies currencies={myListCurrencies} removeFromMyList /> :
+        <DisplayCurrencies currencies={null} removeFromMyList />}
     </div>
   );
 };
