ae21276affc080ef2d756b4c6df1270737e3c1a0
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-04-01 18:30:33 +0300

    Add remove functionality for currencies in DisplayCurrencies component

--- src/components/display-currencies/DisplayCurrencies.tsx
+++ src/components/display-currencies/DisplayCurrencies.tsx
@@ -7,19 +7,27 @@ import { CurrencyState } from "../../state/currency/currencySlice";
 
 import Loading from "../loading/Loading";
 import NoCurrencies from "../no-currencies/NoCurrencies";
+import { addRemoveToMyList, filterMyList } from "../../helper/local-storage/localStorage.helper";
 
 type TopTenCurrenciesProps = {
   currencies: CurrencyState[] | null;
+  removeFromMyList?: boolean;
 };
 
-const DisplayCurrencies = ({ currencies }: TopTenCurrenciesProps) => {
+const DisplayCurrencies = ({ currencies, removeFromMyList }: TopTenCurrenciesProps) => {
 
   const exchangeRates: ExchangeState = useSelector((state: RootState) => state.exchange || {});
+  const myList = useSelector((state: RootState) => state.myList || []);
   const dispatch = useDispatch<AppDispatch>();
 
   function handleValueChange(e: React.ChangeEvent<HTMLInputElement>, id: string) {
     e.preventDefault();
     dispatch({ type: 'exchange/setExchangeRate', payload: { id, value: e.target.value } });
+  }
+  function handleRemoveFromMyList(e: React.MouseEvent<HTMLButtonElement>, currencyId: string) {
+    e.preventDefault();
+    addRemoveToMyList(currencyId);
+    dispatch({ type: 'myList/removeFromMyList', payload: { id: currencyId } });
   }
 
   return (
@@ -27,8 +35,8 @@ const DisplayCurrencies = ({ currencies }: TopTenCurrenciesProps) => {
       <p className="text-xl text-center">{BASE_CURRENCY_TEXT}{BASE_CURRENCY}</p>
       <ul className="flex flex-col gap-3 align-middle justify-evenly p-2">
         {
-          Array.isArray(currencies) && currencies.length > 0 ? currencies.map((currency: CurrencyState) => {
-            return (
+          Array.isArray(currencies) && currencies.length > 0 ? currencies.map((currency: CurrencyState) =>
+            filterMyList(currency) && (
               <li
                 key={currency._id}
                 className="grid grid-cols-1 gap-4 md:grid-cols-3 odd:bg-base-200 even:bg-base-100 p-2 m-2 rounded-2xl shadow-accent-content shadow-2xl">
@@ -50,12 +58,23 @@ const DisplayCurrencies = ({ currencies }: TopTenCurrenciesProps) => {
                     onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleValueChange(e, currency._id)}
                     className="w-1/3  text-center border rounded-2xl p-4" />
                 </div>
-                <div className="flex flex-col text-center justify-center text-xl">
-                  {exchangeRates[currency._id] ? `${(exchangeRates[currency._id] / +(currency.exchangeRateToOneUSD.toFixed(4))).toFixed(2)} ${currency.nameShort}` : `0 ${currency.nameShort}`}
+                <div className="flex flex-row text-center justify-end items-center gap-6 p-4 text-xl">
+                  {exchangeRates[currency._id] ?
+                    `${(exchangeRates[currency._id] / +(currency.exchangeRateToOneUSD.toFixed(4))).toFixed(2)} ${currency.nameShort}` :
+                    `0 ${currency.nameShort}`}
+                  {removeFromMyList && (
+                    <div className="items-center justify-center text-center">
+                      <button
+                        className="btn btn-error btn-outline btn-sm"
+                        onClick={(e) => { handleRemoveFromMyList(e, currency._id) }}>
+                        Remove from favorites
+                      </button>
+                    </div>
+                  )}
                 </div>
               </li>
             )
-          }) : Array.isArray(currencies) ? <Loading /> : <NoCurrencies />
+          ) : Array.isArray(currencies) ? <Loading /> : <NoCurrencies />
         }
       </ul>
     </section>
