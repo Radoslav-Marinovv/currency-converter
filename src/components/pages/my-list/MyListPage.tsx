e9975c0386c6d9b4144c818479e75a02c8d169b3
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-04-01 18:30:43 +0300

    Integrate CarouselCurrencies component and enhance MyListPage layout

--- src/components/pages/my-list/MyListPage.tsx
+++ src/components/pages/my-list/MyListPage.tsx
@@ -2,15 +2,25 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../../state/store";
 
 import DisplayCurrencies from "../../display-currencies/DisplayCurrencies";
+import CarouselCurrencies from "../../carousel-currencies/CarouselCurrencies";
+
 import { getMyList } from "../../../helper/local-storage/localStorage.helper";
+import { useEffect } from "react";
 
 const MyListPage = () => {
   const currencies = useSelector((state: RootState) => state.currencies || {});
+  const myList = localStorage.getItem('myList') || '';
+
+
+  useEffect(() => {
+
+  }, [myList]);
 
   return (
-    <div className="flex flex-col gap-4 ">
+    <div className="flex flex-col gap-4" >
       <h1 className="flex flex-col text-3xl p-3">My List</h1>
-      <DisplayCurrencies currencies={getMyList(currencies)} />
+      <CarouselCurrencies />
+      <DisplayCurrencies currencies={getMyList(currencies)} removeFromMyList />
     </div>
   );
 };
