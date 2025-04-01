bcc84a8fc5a6d24921fc3072721c4e97acd7fe6b
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-04-01 18:30:56 +0300

    Add myList reducer to the Redux store configuration

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import currencyReducer from './currency/currencySlice';
 import exchangeReducer from './exchange/exchangeSlice';
+import { myListReducer } from './myList/myListSlice';
 
 export const store = configureStore({
   reducer: {
     currencies: currencyReducer,
     exchange: exchangeReducer,
+    myList: myListReducer,
   },
 });
 
