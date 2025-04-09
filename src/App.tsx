b92a13161db3f44e11ae1276a696aa5032642194
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-04-09 18:17:27 +0300

    Add route for AllCurrenciesPage in App component

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import MyListPage from "./components/pages/my-list/MyListPage"
 import ErrorNotFoundPage from "./components/pages/error/ErrorNotFoundPage"
 
 import { TIME_TO_UPDATE_IN_MS } from "./constants/constants"
+import AllCurrenciesPage from "./components/pages/all-currencies/AllCurrenciesPage"
 
 function App() {
   const currencies: CurrencyState[] = useSelector((state: RootState) => state.currencies || []);
@@ -38,6 +39,7 @@ function App() {
       children: [
         { index: true, element: <HomePage /> },
         { path: '/my-list', element: <MyListPage /> },
+        { path: '/all-currencies', element: <AllCurrenciesPage /> },
         { path: '*', element: <ErrorNotFoundPage /> }
       ],
       errorElement: <ErrorNotFoundPage />
