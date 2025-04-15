72ba10731c1499ea2305beabcbad32cefb73b8f0
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-04-15 21:35:13 +0300

    Add CarouselInfo component to HomePage for enhanced currency display

--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -5,6 +5,7 @@ import { RootState } from "../../../state/store"
 import { CurrencyState } from "../../../state/currency/currencySlice";
 
 import DisplayCurrencies from "../../display-currencies/DisplayCurrencies";
+import CarouselInfo from "../../carousel-info/CarouselInfo";
 
 const HomePage = () => {
   const currencies: CurrencyState[] = useSelector((state: RootState) => state.currencies || []);
@@ -21,7 +22,8 @@ const HomePage = () => {
     <div className="flex flex-col gap-4 pb-10">
       <h1 className="text-2xl text-warning text-center">NON COMMERCIAL PROJECT - USED FOR EDUCATIONAL PURPOSES ONLY</h1>
       <h2 className="text-4xl text-center">Currency Converter</h2>
-      <p className="text-2xl text-center">API used:  <Link to={'https://freecurrencyapi.com/'} target="_blank" rel="noopener noreferrer">https://freecurrencyapi.com/</Link></p>
+      <p className="text-2xl text-center">API used:  <Link to={'https://freecurrencyapi.com/'} target="_blank" rel="noopener noreferrer" className="link lg:link-hover">https://freecurrencyapi.com/</Link></p>
+      <CarouselInfo />
       <DisplayCurrencies currencies={topTenCurrencies} />
     </div >
   )
